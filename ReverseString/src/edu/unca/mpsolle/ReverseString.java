5774a3b78b881f13741d3aaa8a124f35264ae2a2
mpsolle <denopi@gmail.com>
2013-09-02 11:45:03 -0400

    reverses string and prints out steps

--- ReverseString/src/edu/unca/mpsolle/ReverseString.java
+++ ReverseString/src/edu/unca/mpsolle/ReverseString.java
@@ -1,10 +1,21 @@
 package edu.unca.mpsolle;
 
+import java.util.Scanner;
+
+
+
 public class ReverseString {
+	public static void main(String[] args) {
 
-	public static void main (String[] args) {
-		
+	String source = "";
+	Scanner input = new Scanner(System.in);
+	String stringInput = input.nextLine();
+	int length = stringInput.length();
+	    
+	for(int i = length - 1; i >= 0; i--) {
+	    	source = source + stringInput.charAt(i);
+	    	System.out.println(source);
+	    }
 	}
-
 	
 }
